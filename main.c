5044bfaafc2b6a8565e0c348c5b689f08f8c8cd3
Chunyu Ou <cou5@mail.ccsf.edu>
2017-11-18 22:43:26 -0800

    exercise 8

--- main.c
+++ main.c
@@ -10,6 +10,7 @@
 #include "basic_exercises/exercise_5.h"
 #include "basic_exercises/exercise_6.h"
 #include "basic_exercises/exercise_7.h"
+#include "basic_exercises/exercise_8.h"
 
 int main() {
 
@@ -20,6 +21,7 @@ int main() {
     run_exercise(5, &exercise_5);
     run_exercise(6, &exercise_6);
     run_exercise(7, &exercise_7);
+    run_exercise(8, &exercise_8);
 
     return 0;
 }
